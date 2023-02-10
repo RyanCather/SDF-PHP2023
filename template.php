d14c3bbd79b1b260d2de10950e2b53d65437c5ce
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-02-10 14:14:36 +1100

    footer code

--- template.php
+++ template.php
@@ -1,1 +1,8 @@
 <?php
+
+function footer(): string
+{
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
+}
