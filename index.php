5c1efe95690f95ef47ba853b6658e428db2c2d31
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-02-10 14:29:32 +1100

    output footer with correct timezone

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<?php include "template.php" ?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Heading</h1>
+
+<?php echo footer() ?>
+</body>
+<script src="js/bootstrap.bundle.min.js" ></script>
+</html>
--- template.php
+++ template.php
@@ -2,6 +2,7 @@
 
 function footer(): string
 {
+    date_default_timezone_set('Australia/Canberra');
     $filename = basename($_SERVER["SCRIPT_FILENAME"]);
     $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
     return $footer;
