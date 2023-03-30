7b3f450564b67b5bb73990a21f3a9ea9241524dc
Ryan Cather <ryan.cather@ed.act.edu.au>
2023-03-31 09:39:22 +1100

    navbar changes

--- template.php
+++ template.php
@@ -1,5 +1,7 @@
-<!doctype html>
-<html lang="en">
+<?php session_start(); ?>
+
+    <!doctype html>
+    <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,23 +29,32 @@
                 <li class="nav-item">
                     <a class="nav-link" href="invoiceList.php">Invoice List</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="register.php">Register</a>
-                </li>
+                <?php
+                if (!isset($_SESSION["FirstName"])) {
+                    echo '<li class="nav-item">
+                  <a class="nav-link" href="register.php">Register</a>
+                </li>';
+                }
+                ?>
+
             </ul>
         </div>
+        <?php
+        if (isset($_SESSION["FirstName"])) {
+            echo '<div class="bg-light">Welcome, ' . $_SESSION["FirstName"] . '!<a class="nav-link" href="logout.php">Logout</a></div>';
+        }
+        ?>
     </div>
 </nav>
 
 <?php
 
-session_start();
 
 $conn = new SQLite3("db") or die("Unable to open database");
 
 
-$productNames = array("product1"=>"Darth Vader Helmet", "product2"=>"Grogu Plush", "product3"=>"ROTJ Jigsaw", "product4"=>"Aftermath", "product5"=>"Alphabet Squadron");
-$productPrices= array("product1"=>299.0, "product2"=>32.95, "product3"=>219.95, "product4"=>24.95, "product5"=>25.95);
+$productNames = array("product1" => "Darth Vader Helmet", "product2" => "Grogu Plush", "product3" => "ROTJ Jigsaw", "product4" => "Aftermath", "product5" => "Alphabet Squadron");
+$productPrices = array("product1" => 299.0, "product2" => 32.95, "product3" => 219.95, "product4" => 24.95, "product5" => 25.95);
 function footer(): string
 {
     date_default_timezone_set('Australia/Canberra');
